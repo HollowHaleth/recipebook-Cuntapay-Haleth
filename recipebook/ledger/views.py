172849c580513223165f3caba1c4393eebde13a5
HollowHaleth <HollowHaleth@gmail.com>
2025-03-07 23:22:21 +0800

    [Haleth]: [views.py] changed views to use models

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,41 +1,10 @@
 from django.shortcuts import render
-
-recipes_data = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {"name": "tomato", "quantity": "3pcs"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "pork", "quantity": "1kg"},
-                {"name": "water", "quantity": "1L"},
-                {"name": "sinigang mix", "quantity": "1 packet"},
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {"name": "garlic", "quantity": "1 head"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "vinegar", "quantity": "1/2cup"},
-                {"name": "water", "quantity": "1 cup"},
-                {"name": "salt", "quantity": "1 tablespoon"},
-                {"name": "whole black peppers", "quantity": "1 tablespoon"},
-                {"name": "pork", "quantity": "1 kilo"},
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
+from .models import Recipe
 
 def recipe_list(request):
-    return render(request, 'recipe_list.html', recipes_data)
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe_list.html', {'recipes': recipes})
 
-def recipe_1(request):
-    recipe = recipes_data["recipes"][0]
-    return render(request, 'recipes.html', {"recipe": recipe})
-
-def recipe_2(request):
-    recipe = recipes_data["recipes"][1]
-    return render(request, 'recipes.html', {"recipe": recipe})
+def recipe(request, id):
+    recipe = Recipe.objects.get(id=id) 
+    return render(request, 'recipes.html', {'recipe': recipe})
