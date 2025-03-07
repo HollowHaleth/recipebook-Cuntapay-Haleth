0bbb63d8cb506f734220afa71d4d173014f9c2ca
HollowHaleth <HollowHaleth@gmail.com>
2025-03-07 23:20:05 +0800

    [Haleth]: [models.py] created 3 models (Ingredients, Recipe, RecipeIngredient)

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+class RecipeIngredient(models.Model):
+    #do
+    name
