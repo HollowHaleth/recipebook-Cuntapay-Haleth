d2b60835d88612e1e40f87cef55aeb6f9990d39a
HollowHaleth <HollowHaleth@gmail.com>
2025-03-07 23:20:43 +0800

    [Haleth]: [models.py] added Quantity, Ingredient, and Recipe fields in RecipeIngredient

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -7,5 +7,6 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
 class RecipeIngredient(models.Model):
-    #do
-    name
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
