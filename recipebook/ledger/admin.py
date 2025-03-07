cc7fd7b8e95cf1f08ed7baff0812f4131b26f8aa
HollowHaleth <HollowHaleth@gmail.com>
2025-03-07 23:25:57 +0800

    [Haleth]: [admin.py] added search fields and inline

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
+class RecipeIngredientInline(admin.TabularInline):  
+    model = RecipeIngredient  
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient  
-    
+    list_display = ("name",) 
+    search_fields = ("name",) 
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe  
+    list_display = ("name",) 
+    search_fields = ("name",) 
+    inlines = [RecipeIngredientInline] 
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
