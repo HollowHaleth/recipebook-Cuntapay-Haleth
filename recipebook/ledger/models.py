ed6d59bbe2017f622e0ec3317c4f9746d2f0fd68
HollowHaleth <HollowHaleth@gmail.com>
2025-03-07 23:21:13 +0800

    [Haleth]: [models.py] added string representation and absolute URLs of the Ingredient and Recipe models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,9 +3,21 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    def link(self):
+        return reverse("ledger:recipe_list")
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
+    def link(self):
+        return reverse("ledger:recipe", args=[str(self.id)])
+    
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
