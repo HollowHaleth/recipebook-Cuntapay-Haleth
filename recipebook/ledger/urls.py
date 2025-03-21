c3777317d5e03eacedac9e4687fcc7b4cb06388d
HollowHaleth <HollowHaleth@gmail.com>
2025-03-21 21:12:08 +0800

    [Haleth] [urls.py]: Added authentication URL patterns. Updated URL patterns for authentication views.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+
 app_name = "ledger"
+
 
 urlpatterns = [
     path('recipes/list/', views.recipe_list, name='recipe_list'),
     path('recipe/<int:id>/', views.recipe, name='recipe'),
+    path('accounts/login/', views.custom_login, name='login'),
+    path('accounts/logout/', views.custom_logout, name='logout'),
 ]
