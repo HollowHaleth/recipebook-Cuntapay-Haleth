d116d740c8441846e1137cb3e9f62f30bf49aae6
HollowHaleth <HollowHaleth@gmail.com>
2025-03-21 21:12:05 +0800

    [Haleth] [admin.py]: Registered Profile model. Updated Recipe admin.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,19 +1,29 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
 
-class RecipeIngredientInline(admin.TabularInline):  
-    model = RecipeIngredient  
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
 
 class IngredientAdmin(admin.ModelAdmin):
-    model = Ingredient  
-    list_display = ("name",) 
-    search_fields = ("name",) 
+    model = Ingredient
+    list_display = ("name",)
+    search_fields = ("name",)
 
 class RecipeAdmin(admin.ModelAdmin):
-    model = Recipe  
-    list_display = ("name",) 
-    search_fields = ("name",) 
-    inlines = [RecipeIngredientInline] 
+    model = Recipe
+    list_display = ("name", "author", "created_on", "updated_on")
+    search_fields = ("name", "author__username")
+    list_filter = ("author", "created_on")
+    inlines = [RecipeIngredientInline]
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
@@ -21,6 +31,9 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     list_filter = ("recipe", "ingredient",)
     search_fields = ("recipe__name", "ingredient__name",)
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
