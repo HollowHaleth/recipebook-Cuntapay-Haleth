5a9190245f569e6eb0fd9b35cc8bb180dc8971cb
HollowHaleth <HollowHaleth@gmail.com>
2025-03-21 21:12:06 +0800

    [Haleth] [models.py]: Added Profile model extending User. Updated Recipe model with author and timestamps.

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,23 +1,44 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+
     def __str__(self):
         return self.name
+
 
     def link(self):
         return reverse("ledger:recipe_list")
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+   
     def __str__(self):
         return self.name
 
+
     def link(self):
-        return reverse("ledger:recipe", args=[str(self.id)])
-    
+        return reverse("ledger:recipe", args=[self.id])
+
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
