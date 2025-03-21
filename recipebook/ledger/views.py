ae5f86c7113c1966b31202ff4d3486d541f85003
HollowHaleth <HollowHaleth@gmail.com>
2025-03-21 21:12:07 +0800

    [Haleth] [views.py]: Implemented custom login/logout flow. Restricted Recipe views to authenticated users. Redirected login to recipe list; logout redirects to login.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
+
+
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes})
 
+
+@login_required
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id) 
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipe.html', {'recipe': recipe})
+
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('ledger:recipe_list')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('ledger:recipe_list')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('ledger:login')
+
+
