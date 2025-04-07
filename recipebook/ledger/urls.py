798b9b252316e68315361862d830656f511d32d0
HollowHaleth <HollowHaleth@gmail.com>
2025-04-07 23:55:49 +0800

    [urls.py]: Added URLs for creating new recipes and uploading images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "ledger"
 
 
 urlpatterns = [
     path('recipes/list/', views.recipe_list, name='recipe_list'),
-    path('recipe/<int:id>/', views.recipe, name='recipe'),
+    path('recipe/<int:pk>/', views.recipe, name='recipe'),
     path('accounts/login/', views.custom_login, name='login'),
     path('accounts/logout/', views.custom_logout, name='logout'),
+    path('recipe/add/', views.add_recipe, name='add_recipe'),
+    path('recipe/<int:pk>/add_image/', views.add_recipe_image, name='add_recipe_image'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
