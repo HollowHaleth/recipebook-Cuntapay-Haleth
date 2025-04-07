d64a78b0690f467ad42668d8f406f54763d719f5
HollowHaleth <HollowHaleth@gmail.com>
2025-04-07 23:55:44 +0800

    [views.py]: Configured views to handle creating new recipes and uploading images to recipes.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
-from .models import Recipe
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Recipe, Ingredient, RecipeIngredient, RecipeImage
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-
+from django.forms import modelform_factory, inlineformset_factory
 
 
 @login_required
@@ -13,10 +13,9 @@ def recipe_list(request):
 
 
 @login_required
-def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+def recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipe.html', {'recipe': recipe})
-
 
 def custom_login(request):
     if request.user.is_authenticated:
@@ -33,12 +32,68 @@ def custom_login(request):
                 return redirect('ledger:recipe_list')
     else:
         form = AuthenticationForm()
-    
-    return render(request, 'registration/login.html', {'form': form})
 
+    return render(request, 'registration/login.html', {'form': form})
 
 def custom_logout(request):
     logout(request)
     return redirect('ledger:login')
 
 
+@login_required
+def add_recipe(request):
+    recipes = Recipe.objects.all()  
+    ingredients = Ingredient.objects.all()  
+
+    if request.method == 'POST':
+        
+        if 'create_recipe' in request.POST:
+            recipe_name = request.POST.get('recipe_name')
+            recipe = Recipe.objects.create(name=recipe_name, author=request.user)
+
+        
+        if 'create_ingredient' in request.POST:
+            ingredient_name = request.POST.get('ingredient_name')
+            ingredient = Ingredient.objects.create(name=ingredient_name)
+
+        
+        if 'create_recipe_ingredient' in request.POST:
+            quantity = request.POST.get('quantity')
+            ingredient_id = request.POST.get('ingredient')
+            recipe_id = request.POST.get('recipe')
+
+            
+            ingredient = Ingredient.objects.get(id=ingredient_id)
+            recipe = Recipe.objects.get(id=recipe_id)
+
+            
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient=ingredient,
+                quantity=quantity
+            )
+
+            return redirect('ledger:recipe_list')  
+
+    return render(request, 'add_recipe.html', {
+        'recipes': recipes,
+        'ingredients': ingredients
+    })
+
+
+@login_required
+def add_recipe_image(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    RecipeImageForm = modelform_factory(RecipeImage, fields=['image', 'description'])
+
+    if request.method == 'POST':
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe_image = form.save(commit=False)
+            recipe_image.recipe = recipe
+            recipe_image.save()
+            return redirect('ledger:recipe', pk=recipe.pk)
+    else:
+        form = RecipeImageForm()
+
+    return render(request, 'add_image.html', {'form': form, 'recipe': recipe})
